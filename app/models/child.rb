5bdc77e1926d26d9fb1268da8d795b997bdf04c1
Andey <andeyn@andrew.cmu.edu>
2021-02-07 23:32:38 -0500

    added validate first/last name

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,20 @@
 class Child < ApplicationRecord
-    
-end
+    # Relationships
+    # -----------------------------
+    has_many :chores
+    has_many :tasks, through: :chores
+  
+    #validates
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+  
+    #methods
+    def name
+        return "#{first_name} #{last_name}" #last_name+" "+
+    end
+  
+    #scopes
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :active, -> { where(active: true) }
+  end
+  
